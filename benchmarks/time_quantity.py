c96db44338bda939283b7677c640d3324c46986d
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-01-09 21:04:56 +0100

    Added conversion tests

--- benchmarks/time_quantity.py
+++ benchmarks/time_quantity.py
@@ -10,5 +10,13 @@ def time_quantity_init_array():
     np.arange(100000) * u.m / u.s
 
 
+def time_quantity_scalar_conversion():
+    (3. * u.m / u.s).to(u.km / u.hour)
+
+
+def time_quantity_array_conversion():
+    (np.arange(100000) * u.m / u.s).to(u.km / u.hour)
+
+
 def time_quantity_ufunc_sin():
     np.sin(np.arange(10000) * u.degree)
