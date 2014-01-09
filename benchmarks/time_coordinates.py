4737e41f7230a751d7d26e04c616c3f53d95ae33
Michael Droettboom <mdboom@gmail.com>
2014-01-09 14:26:34 -0500

    Initial commit astropy benchmarks

---
+++ benchmarks/time_coordinates.py
@@ -0,0 +1,6 @@
+from astropy import coordinates
+from astropy import units
+
+
+def time_latitude():
+    coordinates.Latitude(3.2, units.degree)
---
+++ benchmarks/time_units.py
@@ -0,0 +1,25 @@
+from astropy import units as u
+
+
+def time_unit_compose():
+    u.Ry.compose()
+
+
+def time_unit_to():
+    u.m.to(u.pc)
+
+
+def time_unit_parse():
+    u.Unit('1e-07 kg m2 / s2')
+
+
+def time_simple_unit_parse():
+    u.Unit('1 d')
+
+
+def time_very_simple_unit_parse():
+    u.Unit('d')
+
+
+def mem_unit():
+    return u.erg
