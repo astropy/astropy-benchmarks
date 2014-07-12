ab13c0b907888bb27244ecc070f20f42b9bb93ac
Michael Mueller <michaeldmueller7@gmail.com>
2014-07-12 19:00:21 -0400

    Added benchmarks related to io.ascii

---
+++ benchmarks/time_sextractor.py
@@ -0,0 +1,22 @@
+from astropy.io.ascii import sextractor
+import random
+import string
+
+def randword():
+    return ''.join([random.choice(string.uppercase) for i in range(10)])
+    
+class SExtractorSuite:
+    def setup(self):
+        self.header = sextractor.SExtractorHeader()
+        self.lines = []
+        i = 0
+        while i < 100000:
+            if i % 20 == 0 and i != 0:
+                i += 4
+            i += 1
+            self.lines.append('# {} {} Description [pixel**2]'.format(
+                                                        i, randword()))
+        self.lines.append('Non-header line')
+
+    def time_header(self):
+        self.header.get_cols(self.lines)
