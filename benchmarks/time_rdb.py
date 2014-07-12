0249a3f80cce6d7acb24013649aee7017def7430
Michael Mueller <michaeldmueller7@gmail.com>
2014-07-12 19:00:21 -0400

    Added benchmarks related to io.ascii

---
+++ benchmarks/time_rdb.py
@@ -0,0 +1,11 @@
+from astropy.io.ascii import basic
+
+class RDBSuite:
+    def setup(self):
+        self.header = basic.RdbHeader()
+        self.header.splitter.delimiter = '\t'
+        f = open('benchmarks/files/rdb/string.txt')
+        self.lines = f.read().split('\n')
+        f.close()
+    def time_get_cols(self):
+        self.header.get_cols(self.lines)
