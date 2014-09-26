00f0647beec3370a207e6301f29c8eaee7b3f48c
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-09-26 12:18:55 +0200

    Added benchmarks following suggestions by @mhvk and @taldcroft

--- benchmarks/time_table.py
+++ benchmarks/time_table.py
@@ -6,10 +6,12 @@ from astropy.table import Table
 
 class TimeTable:
 
+    masked = False
+
     def setup(self):
 
         # Initialize table
-        self.table = Table()
+        self.table = Table(masked=self.masked)
 
         # Create column with mixed types
         np.random.seed(12345)
@@ -22,10 +24,33 @@ class TimeTable:
 
         self.extra_column = np.random.randint(0, 100, 1000)
 
-    def time_column_access(self):
+        self.row_indices = np.where(self.table['a'] > 0.9)[0]
+
+    def time_table_slice_bool(self):
+        table_subset = self.table[self.table['a'] > 0.6]
+
+    def time_table_slice_int(self):
+        table_subset = self.table[self.row_indices]
+
+    def time_column_slice_bool(self):
+        col_subset = self.table['a'][self.table['a'] > 0.6]
+
+    def time_column_slice_int(self):
+        col_subset = self.table['a'][self.row_indices]
+
+    def time_column_get(self):
         self.table['c']
 
-    def time_row_access(self):
+    def time_column_set(self):
+        self.table['a'] = 0.
+
+    def time_multi_column_get(self):
+        self.table[('a','c')]
+
+    def time_column_set_row_subset(self):
+        self.table['b'][self.table['a'] > 0.6] = True
+
+    def time_row_get(self):
         self.table[300]
 
     def time_iter_row(self):
@@ -35,5 +60,32 @@ class TimeTable:
     def time_add_row(self):
         self.table.add_row(self.extra_row)
 
+    def time_remove_row(self):
+        self.table.remove_row(6)
+    time_remove_row.repeat = 1
+
+    def time_remove_rows(self):
+        self.table.remove_rows(self.row_indices)
+    time_remove_rows.repeat = 1
+
     def time_add_column(self):
         self.table['e'] = self.extra_column
+    time_add_column.repeat = 1
+
+    def time_remove_column(self):
+        self.table.remove_column('a')
+    time_remove_column.repeat = 1
+
+    def time_copy_table(self):
+        self.table.copy()
+
+    def time_copy_column(self):
+        self.table['a'].copy()
+
+
+class TimeMaskedTable(TimeTable):
+
+    masked = True
+
+    def time_mask_column(self):
+        self.table['a'].mask = self.table['a'] < 0.2
