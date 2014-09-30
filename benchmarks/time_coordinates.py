e880a42ab6c1ed31f9745485297b761f0aeb10c3
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-09-30 09:16:14 +0200

    Added frame initialization benchmarks

--- benchmarks/time_coordinates.py
+++ benchmarks/time_coordinates.py
@@ -1,17 +1,38 @@
 import numpy as np
-from astropy import coordinates
-from astropy import units
+from astropy import coordinates as coords
+from astropy import units as u
 
 
 def time_latitude():
-    coordinates.Latitude(3.2, units.degree)
+    coords.Latitude(3.2, u.degree)
+
+
+class FrameBenchmarks:
+
+    def setup(self):
+
+        self.scalar_ra = 3.2 * u.deg
+        self.scalar_dec = 2.2 * u.deg
+
+        self.array_ra = np.linspace(0., 360., 1000) * u.deg
+        self.array_dec = np.linspace(-90., 90., 1000) * u.deg
+
+    def time_init_nodata(self):
+        coords.FK5()
+
+    def time_init_scalar(self):
+        coords.FK5(self.scalar_ra, self.scalar_dec)
+
+    def time_init_array(self):
+        coords.FK5(self.array_ra, self.array_dec)
 
 
 class SkyCoordBenchmarks:
+
     def setup(self):
         N = int(1e6)
         lon, lat = np.ones(N), np.ones(N)
-        self.coord = coordinates.SkyCoord(lon, lat, unit='deg', frame='icrs')
+        self.coord = coords.SkyCoord(lon, lat, unit='deg', frame='icrs')
 
     def time_repr(self):
         repr(self.coord)
