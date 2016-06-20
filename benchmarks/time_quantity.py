bc56a12a729e174b3d1e4121b15fb7b2321413d2
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2016-06-20 14:15:22 -0400

    Add some quantity initialization and computation benchmarks.

--- benchmarks/time_quantity.py
+++ benchmarks/time_quantity.py
@@ -2,12 +2,30 @@ import numpy as np
 from astropy import units as u
 
 
+a = np.arange(100000.)
+q0 = u.Quantity(1., u.s)
+q1 = u.Quantity(a, u.m)
+q2 = u.Quantity(a[:10000], u.deg)
+
+
+def time_quantity_creation():
+    u.Quantity(a, u.m)
+
+
+def time_quantity_creation_nocopy():
+    u.Quantity(a, u.m, copy=False)
+
+
+def time_quantity_view():
+    q1.view(u.Quantity)
+
+
 def time_quantity_init_scalar():
     3. * u.m / u.s
 
 
 def time_quantity_init_array():
-    np.arange(100000) * u.m / u.s
+    a * u.m / u.s
 
 
 def time_quantity_scalar_conversion():
@@ -15,8 +33,16 @@ def time_quantity_scalar_conversion():
 
 
 def time_quantity_array_conversion():
-    (np.arange(100000) * u.m / u.s).to(u.km / u.hour)
+    (a * u.m / u.s).to(u.km / u.hour)
+
+
+def time_quantity_times_unit():
+    q1 * u.m
+
+
+def time_quantity_times_quantity():
+    q1 * q0
 
 
 def time_quantity_ufunc_sin():
-    np.sin(np.arange(10000) * u.degree)
+    np.sin(q2)
