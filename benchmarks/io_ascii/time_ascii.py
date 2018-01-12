bc028e501392d187509e4f1adf47458c40d6ec3d
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-01-12 14:44:58 +0000

    Avoid issue with method names with developer version of asv

--- benchmarks/io_ascii/time_ascii.py
+++ benchmarks/io_ascii/time_ascii.py
@@ -46,267 +46,418 @@ class _ASCIISuite:
 class CsvString(_ASCIISuite):
     file_format = 'csv'
     data_type = 'string'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class CsvInt(_ASCIISuite):
     file_format = 'csv'
     data_type = 'int'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class CsvFloat(_ASCIISuite):
     file_format = 'csv'
     data_type = 'float'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class RdbString(_ASCIISuite):
     file_format = 'rdb'
     data_type = 'string'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class RdbInt(_ASCIISuite):
     file_format = 'rdb'
     data_type = 'int'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class RdbFloat(_ASCIISuite):
     file_format = 'rdb'
     data_type = 'float'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class FixedWidthString(_ASCIISuite):
     file_format = 'fixed_width'
     data_type = 'string'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class FixedWidthInt(_ASCIISuite):
     file_format = 'fixed_width'
     data_type = 'int'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class FixedWidthFloat(_ASCIISuite):
     file_format = 'fixed_width'
     data_type = 'float'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class FixedWidthNoHeaderString(_ASCIISuite):
     file_format = 'fixed_width_no_header'
     data_type = 'string'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class FixedWidthNoHeaderInt(_ASCIISuite):
     file_format = 'fixed_width_no_header'
     data_type = 'int'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class FixedWidthNoHeaderFloat(_ASCIISuite):
     file_format = 'fixed_width_no_header'
     data_type = 'float'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class FixedWidthTwoLineString(_ASCIISuite):
     file_format = 'fixed_width_two_line'
     data_type = 'string'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class FixedWidthTwoLineInt(_ASCIISuite):
     file_format = 'fixed_width_two_line'
     data_type = 'int'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class FixedWidthTwoLineFloat(_ASCIISuite):
     file_format = 'fixed_width_two_line'
     data_type = 'float'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class TabString(_ASCIISuite):
     file_format = 'tab'
     data_type = 'string'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class TabInt(_ASCIISuite):
     file_format = 'tab'
     data_type = 'int'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class TabFloat(_ASCIISuite):
     file_format = 'tab'
     data_type = 'float'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class NoHeaderString(_ASCIISuite):
     file_format = 'no_header'
     data_type = 'string'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class NoHeaderInt(_ASCIISuite):
     file_format = 'no_header'
     data_type = 'int'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class NoHeaderFloat(_ASCIISuite):
     file_format = 'no_header'
     data_type = 'float'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class CommentedHeaderString(_ASCIISuite):
     file_format = 'commented_header'
     data_type = 'string'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class CommentedHeaderInt(_ASCIISuite):
     file_format = 'commented_header'
     data_type = 'int'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class CommentedHeaderFloat(_ASCIISuite):
     file_format = 'commented_header'
     data_type = 'float'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class BasicString(_ASCIISuite):
     file_format = 'basic'
     data_type = 'string'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class BasicInt(_ASCIISuite):
     file_format = 'basic'
     data_type = 'int'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class BasicFloat(_ASCIISuite):
     file_format = 'basic'
     data_type = 'float'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class SextractorString(_ASCIISuite):
     file_format = 'sextractor'
     data_type = 'string'
-    time_read = _ASCIISuite.read
+
+    def time_read(self):
+        return self.read()
 
 
 class SextractorInt(_ASCIISuite):
     file_format = 'sextractor'
     data_type = 'int'
-    time_read = _ASCIISuite.read
+
+    def time_read(self):
+        return self.read()
 
 
 class SextractorFloat(_ASCIISuite):
     file_format = 'sextractor'
     data_type = 'float'
-    time_read = _ASCIISuite.read
+
+    def time_read(self):
+        return self.read()
+
 
 class IpacString(_ASCIISuite):
     file_format = 'ipac'
     data_type = 'string'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class IpacInt(_ASCIISuite):
     file_format = 'ipac'
     data_type = 'int'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class IpacFloat(_ASCIISuite):
     file_format = 'ipac'
     data_type = 'float'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class LatexString(_ASCIISuite):
     file_format = 'latex'
     data_type = 'string'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class LatexInt(_ASCIISuite):
     file_format = 'latex'
     data_type = 'int'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class LatexFloat(_ASCIISuite):
     file_format = 'latex'
     data_type = 'float'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class AastexString(_ASCIISuite):
     file_format = 'aastex'
     data_type = 'string'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class AastexInt(_ASCIISuite):
     file_format = 'aastex'
     data_type = 'int'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
 
 
 class AastexFloat(_ASCIISuite):
     file_format = 'aastex'
     data_type = 'float'
-    time_read = _ASCIISuite.read
-    time_write = _ASCIISuite.write
+
+    def time_read(self):
+        return self.read()
+
+    def time_write(self):
+        return self.write()
