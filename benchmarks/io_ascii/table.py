5598f4e265e2d1768dc1805ca2bc7374238361e3
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2019-11-18 13:59:05 -0500

    Fix mem profiling error for ASCII Table

--- benchmarks/io_ascii/table.py
+++ benchmarks/io_ascii/table.py
@@ -17,25 +17,34 @@ class TableSuite:
         self.lst.append([random.randint(-500, 500) for i in range(1000)])
         self.lst.append([random.random() * 500 - 500 for i in range(1000)])
         self.lst.append([''.join([random.choice(uppercase) for j in
-                            range(6)]) for i in range(1000)])
+                                  range(6)]) for i in range(1000)])
         self.cols = [core.Column(str(i + 1)) for i in range(3)]
         for col, x in izip(self.cols, self.lst):
             col.data = x
         self.table_cols = [table.Column(x) for x in self.lst]
         self.outputter = core.TableOutputter()
         self.table = table.Table()
+
     def time_table_outputter(self):
         self.outputter(self.cols, {'table': {}})
+
+    # https://github.com/astropy/astropy-benchmarks/issues/87
     def mem_table_outputter(self):
-        return self.outputter(self.cols, {'table': {}})
+        return self.outputter(self.cols, {'table': {}}).as_array()
+
     def time_str_vals_int(self):
         self.table_cols[0].iter_str_vals()
+
     def time_str_vals_float(self):
         self.table_cols[1].iter_str_vals()
+
     def time_str_vals_str(self):
         self.table_cols[2].iter_str_vals()
+
     def time_table_init_from_list(self):
         self.table._init_from_list(self.table_cols, ['1', '2', '3'],
                                    [None, None, None], 3, True)
+
+    # https://github.com/astropy/astropy-benchmarks/issues/87
     def mem_table_init(self):
-        return table.Table(self.lst)
+        return table.Table(self.lst).as_array()
